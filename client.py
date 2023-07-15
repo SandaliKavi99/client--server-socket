aac0b6c37fd87f0fec0edc2dd51f93cb9b280259
kasun789 <kasungayashan789@gmail.com>
2023-07-15 16:46:08 +0530

    do some changes

--- client.py
+++ client.py
@@ -1,8 +1,27 @@
 import socket
+import sys
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect((socket.gethostname(),6040))
+def startClient(host,port):
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client.connect((socket.gethostname(),port))
 
-message = client.recv(2048)
+    message = client.recv(2048)
+    print(f"Message recived: {message}")
+    while True:
+        msg = input('Enter a message: ')
+        client.send(msg.encode('utf-8'))
+        if(msg == 'terminate'):
+            client.close()
+            break
+    
 
-print(f"Message recived: {message}")
+# main function
+if __name__ == "__main__":
+   if len(sys.argv) != 3:
+      print("Usage: py server.py <server IP> <port>")
+      sys.exit(1)
+   serverIP = sys.argv[1]
+   port = int(sys.argv[2])
+  
+
+   startClient(serverIP,port)
--- server.py
+++ server.py
@@ -1,11 +1,35 @@
 import socket
+import threading
+import sys
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((socket.gethostname(),6040))
-server.listen()
 
-while True:
-   clientSocket, address = server.accept()
-   print(f"Connection established from address {address}")
-   clientSocket.send(bytes("Welcome to the Server!!!!","utf-8"))
-   clientSocket.close()
+# thread for sent msgs
+def createTread(clientSocket,address):
+ 
+   while True:
+      print("client ",address,">")
+      msg =clientSocket.recv(256).decode()
+      if(msg == 'terminate'):
+         break
+      print(msg)
+
+# make server
+def startServer(port):
+   server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   server.bind((socket.gethostname(),port))
+   server.listen()
+
+   while True:
+      clientSocket, address = server.accept()
+      print(f"Connection established from address {address}")
+      clientSocket.send(bytes("Welcome to the Server!!!!","utf-8"))
+      
+      threading.Thread(target=createTread,args=(clientSocket,address)).start()
+
+# main function
+if __name__ == "__main__":
+   if len(sys.argv) != 2:
+      print("Usage: py server.py <port>")
+      sys.exit(1)
+   port = int(sys.argv[1])
+   startServer(port)
